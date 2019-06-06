9861a67b892f4084f8defac688486f1ddaa9436a
iakanykin <iakanykin@gmail.com>
2019-06-06 12:53:16 +0300

    Initial Commit

---
+++ Reference App/Supporting Files/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Reference App
+//
+//  Created by Denis Bystruev on 26/02/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    
+}
+
